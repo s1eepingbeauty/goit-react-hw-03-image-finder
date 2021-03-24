613dfd346031bbd01690fca7bba14b026a39fcb5
s1eepingbeauty <kingz.bridge@gmail.com>
2021-03-24 20:26:59 +0200

    create baseUrl in apiService

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,9 +1,8 @@
 const apiKey = '19951456-e393dc841832362ddd9551c23';
+const baseUrl = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12';
 
 const apiService = (searchQuery, page) => {
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${encodeURIComponent(
-    searchQuery,
-  )}&page=${page}&per_page=12&key=${apiKey}`;
+  const url = `${baseUrl}&q=${encodeURIComponent(searchQuery)}&page=${page}&key=${apiKey}`;
 
   return fetch(url)
     .then((response) => response.json())
